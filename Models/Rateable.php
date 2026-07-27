93fc0f74dd0789f832a32143ba61dc00837c6420
Francesco Cassese <francescocassese9@gmail.com>
2026-07-27 19:14:19 +0200

    fix: rimuove newline superfluo nel messaggio di errore di setVote

--- Models/Rateable.php
+++ Models/Rateable.php
@@ -8,7 +8,7 @@ trait Rateable
     public function setVote(int $_vote)
     {
         if ($_vote < 1 || $_vote > 10) {
-            echo "Il voto deve essere tra 1 e 10, ricevuto: {$_vote}\n";
+            echo "Il voto deve essere tra 1 e 10, ricevuto: {$_vote}";
             return;
         }
         $this->vote = $_vote;
