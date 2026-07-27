d7bb478d3fbec2423852c766ca70cdf5ece49143
Francesco Cassese <francescocassese9@gmail.com>
2026-07-27 18:48:25 +0200

    aggiunge trait Rateable con validazione voto (1-10) e gestione proprietà non inizializzata

---
+++ Models/Rateable.php
@@ -0,0 +1,21 @@
+<?php
+
+trait Rateable
+{
+
+    public int $vote;
+
+    public function setVote(int $_vote)
+    {
+        if ($_vote < 1 || $_vote > 10) {
+            echo "Il voto deve essere tra 1 e 10, ricevuto: {$_vote}\n";
+            return;
+        }
+        $this->vote = $_vote;
+    }
+
+    public function getRatingStars()
+    {
+        return isset($this->vote) ? "Valutazione: {$this->vote}/10" : "Non ancora valutato";
+    }
+}
